800e95aea694535de87bfb8660f6b9fafda5e131
Simon Hintersonnleitner <simon@hintersonnleitner.me>
2023-10-31 16:19:03 +0100

    Fix: reduce amount of items per page to avoid memory issues

--- src/Request/IterableRequest.php
+++ src/Request/IterableRequest.php
@@ -7,7 +7,7 @@ namespace FINDOLOGIC\PlentyMarketsRestExporter\Request;
 trait IterableRequest
 {
     /** Maximum count of entities per page. */
-    public static int $ITEMS_PER_PAGE = 50;
+    public static int $ITEMS_PER_PAGE = 25;
     protected int $page = 1;
 
     public function getParams(): array
