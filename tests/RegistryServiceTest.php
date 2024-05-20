c853e8f969839f554265e8f8ce1f052c46ab086f
aserstobitov <artiom.serstobitov@hashtages.com>
2024-05-20 10:02:38 +0300

    customer exception namespace fix

--- tests/RegistryServiceTest.php
+++ tests/RegistryServiceTest.php
@@ -8,7 +8,7 @@ use Exception;
 use FINDOLOGIC\PlentyMarketsRestExporter\Client;
 use FINDOLOGIC\PlentyMarketsRestExporter\Config;
 use FINDOLOGIC\PlentyMarketsRestExporter\Exception\AuthorizationException;
-use FINDOLOGIC\PlentyMarketsRestExporter\Exception\CustomerException;
+use FINDOLOGIC\PlentyMarketsRestExporter\Exception\Retry\CustomerException;
 use FINDOLOGIC\PlentyMarketsRestExporter\Exception\PermissionException;
 use FINDOLOGIC\PlentyMarketsRestExporter\Exception\Retry\EmptyResponseException;
 use FINDOLOGIC\PlentyMarketsRestExporter\Exception\ThrottlingException;
